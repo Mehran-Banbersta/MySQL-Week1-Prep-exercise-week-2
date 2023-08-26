49cd15ac51a6c31da35b9148cd09ca40658b67d1
mehran banbersta <mehranbanbersta@mehrans-mbp.kpn>
2023-08-26 17:30:17 +0200

    create 	food-recipes.sql file

---
+++ food-recipes.sql
@@ -0,0 +1,26 @@
+
+-- create Recipe table
+CREATE TABLE Recipe (
+  recipe_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(255) NOT NULL
+);
+
+-- create Category table
+CREATE TABLE Category (
+  category_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+   name VARCHAR(255) NOT NULL
+);
+
+-- create Ingredient table 
+CREATE TABLE Ingredient (
+  igredient_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(255) NOT NULL
+);
+
+-- create Step table 
+CREATE TABLE Steps (
+  step_id INT INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  stepDescription TEXT NOT NULL
+);
+
+-- create 
