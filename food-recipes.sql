d1b146f1568858e88b33753b8fff7087118f1fce
mehran banbersta <mehranbanbersta@mehrans-mbp.kpn>
2023-08-26 17:37:29 +0200

    food-recipes.sql updated

--- food-recipes.sql
+++ food-recipes.sql
@@ -1,26 +1,29 @@
-
 -- create Recipe table
 CREATE TABLE Recipe (
   recipe_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(255) NOT NULL
 );
-
 -- create Category table
 CREATE TABLE Category (
   category_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-   name VARCHAR(255) NOT NULL
+  name VARCHAR(255) NOT NULL
 );
-
 -- create Ingredient table 
 CREATE TABLE Ingredient (
   igredient_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(255) NOT NULL
 );
-
 -- create Step table 
 CREATE TABLE Steps (
   step_id INT INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   stepDescription TEXT NOT NULL
 );
-
--- create 
+-- create RecipeCategory table
+CREATE TABLE RecipeCategory ();
+-- create RecipeIngredient table
+CREATE TABLE RecipeIngredient ();
+-- create RecipeStep table
+CREATE TABLE RecipeStep ();
+-- insert into Category
+INSERT INTO Category ()
+VALUES
